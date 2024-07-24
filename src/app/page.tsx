f3c0f51596cb7eff5d349fdd1b87c57db37fb79f
Hano2001 <haraldnorlin93@gmail.com>
2024-07-24 13:49:29 +0200

    -m
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Lukaslaas <lukas.s.laas@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <main>
       <h1>Products</h1>
+      <ul>
+        <li>
+          <span>Product Title</span>
+          <span>Price</span>
+        </li>
+      </ul>
     </main>
   );
 }

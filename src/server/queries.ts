7b4902123a5efa16963bb9586991ba0c0f941516
lukas-laas <lukas.s.laas@gmail.com>
2024-07-24 16:26:42 +0200

    undo incorrect typing

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,20 +1,10 @@
 import "server-only";
 import { db } from "./db";
-type Product = {
-  id: string;
-  title: string;
-  price: number;
-  images?: string[];
-  condition: string;
-  age: number;
-  category: string;
-  initialPrice: number;
-};
 
 export const getProducts = async () => {
   try {
     const products = await db.query.Products.findMany();
-    const productsDTO = products.map((product: Product) => {
+    const productsDTO = products.map((product) => {
       return { id: product.id, title: product.title, price: product.price };
     });
     return productsDTO;

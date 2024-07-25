8ce9b4dae451dae22780dd0023be2cd6e5e8cc83
Oriana Mendez <egleemendez@gmail.com>
2024-07-25 15:06:27 +0200

    create table for product page
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
 import { getProducts } from "@/server/queries";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
 
 export default async function Home() {
   const products = await getProducts();
   return (
     <main>
       <h1>Products</h1>
-      <ul>
-        {products.map((product) => {
-          return (
-            <li key={product.id}>
-              <span>{product.title}: </span>
-              <span>{product.price} sek</span>
-            </li>
-          );
-        })}
-      </ul>
+      <DataTable columns={columns} data={products} />
     </main>
   );
 }

f0777cca59b87d231898273be4b2c58813faad5c
Oriana Mendez <egleemendez@gmail.com>
2024-07-25 15:05:57 +0200

    add favicon and logo
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/_components/top-nav.tsx
+++ src/app/_components/top-nav.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+
 export const TopNav = () => {
   return (
     <nav className="bg-gray-800 flex justify-between items-center p-2">
+      <Image
+        src="/logo-shmecond.png"
+        width={100}
+        height={100}
+        alt="Shmecond Hand Logo"
+      />
       <div>Schmecond Hand</div>
       <button className="rounded-md p-2 hover:bg-gray-700 ">Add</button>
     </nav>

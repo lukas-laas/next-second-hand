e05617fbf9fc8d74a045acd56769b6679100434b
Oriana Mendez <egleemendez@gmail.com>
2024-07-25 13:50:00 +0200

    product/id skeleton
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,3 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function ProductDetailPage() {
-  return <h1>Product</h1>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Product Title</CardTitle>
+        {/* <CardDescription>Card Description</CardDescription> */}
+      </CardHeader>
+      <CardContent>
+        <p>Price</p>
+        <p>Original price</p>
+        <p>Condition</p>
+        <p>Category</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
 }
